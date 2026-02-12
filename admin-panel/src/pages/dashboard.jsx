e89043e85c426d40a9e55646515be80c1d83c5d9
Gomathisanjeevi <gomathisanjeevi06@gmail.com>
2026-02-12 16:42:28 +0530

    dashboard.jsx

--- admin-panel/src/pages/dashboard.jsx
+++ admin-panel/src/pages/dashboard.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-const SOCKET_URL = "http://192. 168.0.3:3000";
+const SOCKET_URL = "http://192.168.0.3:3000";
 
 
 export default function Dashboard() {
